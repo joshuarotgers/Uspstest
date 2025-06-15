8708e7504571f2bd6da25a3c2681ce7ef561ea74
joshuarotgers <joshua.rotgers@gmail.com>
2025-06-15 07:12:57 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
 
+module.exports = mongoose.model("User", new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["carrier", "supervisor", "district-admin", "hq"] },
+  district: String
+}));
